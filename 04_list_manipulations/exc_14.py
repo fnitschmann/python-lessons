2c20b83c2f700961b9df1d113c8b0b2e87555285
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 12:32:51 +0200

    exercise 14

---
+++ 04_list_manipulations/exc_14.py
@@ -0,0 +1,6 @@
+# list manipulations - exercise 14
+invited_persons = ["Daniel", "Mark", "Hunter"]
+
+for person in invited_persons:
+    message = "Hey {}! You are invited to my dinner.".format(person)
+    print(message)
