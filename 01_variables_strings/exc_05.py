a74d5f9ff19381231956eebdcbfb65a3f7f544c0
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 11:49:56 +0200

    exercise 05

---
+++ 01_variables_strings/exc_05.py
@@ -0,0 +1,7 @@
+# variables and strings exercise 5
+author = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+
+sentence = "{0} said once: \"{1}\"".format(author, quote)
+
+print(sentence)
