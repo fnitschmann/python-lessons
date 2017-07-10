a8c96b5731308cb947c50eb28c754d04a398a1c0
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 14:27:02 +0200

    exercise 21

---
+++ 05_working_with_lists/exc_21.py
@@ -0,0 +1,8 @@
+# working with lists - exercise 21
+pizzas = ["mista", "salami", "pepperoni"]
+
+for pizza in pizzas:
+    message = "I really like pizza {}.".format(pizza)
+    print(message)
+
+print("I really love pizza!")
