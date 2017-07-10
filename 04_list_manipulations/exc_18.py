171a24363597a94477665fc1ee63e87fbdd5a9cf
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 13:31:37 +0200

    exercise 18

---
+++ 04_list_manipulations/exc_18.py
@@ -0,0 +1,21 @@
+# list manipulations - exercise 18
+cities = ["St. Petersburg", "Moscow", "Buenos Aires", "New York", "Stockholm",
+        "Amsterdam"]
+
+print(cities)
+print(sorted(cities))
+print(cities)
+print(sorted(cities, reverse = True))
+print(cities)
+
+cities.reverse()
+print(cities)
+
+cities.reverse()
+print(cities)
+
+cities.sort()
+print(cities)
+
+cities.sort(reverse = True)
+print(cities)
