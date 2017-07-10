7568a4ae585d9aed6edd3e9d71c95744db195b0b
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 15:49:17 +0200

    exercise 63

---
+++ 07_functions/exc_63.py
@@ -0,0 +1,8 @@
+# functions - exercise 63
+def make_shirt(size, message):
+    sentence = "Your shirt has the message '{}' and the size is {}.".format(message, size)
+
+    print(sentence)
+
+make_shirt("L", "Manchester United is cool")
+make_shirt(message = "A cool message", size = "M")
