9079ac0575dd23aaaef19dcb0517eefdd446a4d8
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 15:51:03 +0200

    adjuste wihtespace in exercise 63

--- 07_functions/exc_63.py
+++ 07_functions/exc_63.py
@@ -1,7 +1,6 @@
 # functions - exercise 63
 def make_shirt(size, message):
     sentence = "Your shirt has the message '{}' and the size is {}.".format(message, size)
-
     print(sentence)
 
 make_shirt("L", "Manchester United is cool")
