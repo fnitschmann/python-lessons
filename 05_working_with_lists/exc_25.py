27c9b3e404c5e90e31f61d16215f19b90ad276f0
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 14:39:42 +0200

    exercise 25

---
+++ 05_working_with_lists/exc_25.py
@@ -0,0 +1,10 @@
+# working with lists - exercise 25
+numbers = range(1, 1000001)
+
+min_number = min(numbers)
+max_number = max(numbers)
+sum_numbers = sum(numbers)
+
+print("Min number: {}".format(min_number))
+print("Max number: {}".format(max_number))
+print("Sum of all numbers: {}".format(sum_numbers))
