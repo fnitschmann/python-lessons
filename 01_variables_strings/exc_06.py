04c722db7cbe37bbe77acfbfd1f84448a538f3af
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-10 11:51:44 +0200

    exercise 06

---
+++ 01_variables_strings/exc_06.py
@@ -0,0 +1,7 @@
+# variables and strings exercise 6
+famous_person = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+
+message = "{0} said once: \"{1}\"".format(famous_person, quote)
+
+print(message)
