85d165786e13d535cac7497857f4cb70e5ba8949
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-13 01:09:53 +0200

    exercise 43

---
+++ 07_dictionaries/exc_43.py
@@ -0,0 +1,10 @@
+# dictionaries - exercise 43
+person_data = {
+        "first_name": "Mohamed",
+        "last_name": "Lee",
+        "age": 22,
+        "city": "Tokyo"
+        }
+
+for key, value in person_data.items():
+    print("{}: {}".format(key, value))
